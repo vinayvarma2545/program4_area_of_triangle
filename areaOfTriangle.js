a63a215ddfb8010e60b97bd2e64c34af60eb0955
vinay <vinayvarma2545@gmail.com>
2022-09-17 14:52:41 +0530

    area of triangle

---
+++ areaOfTriangle.js
@@ -0,0 +1,4 @@
+const base = parseInt(prompt('enter base triangle:'));
+const height = parseInt(prompt('enter height triangle:'));
+const area = (base*height)/2;
+document.write(`area of triangle is : ${area}`)
